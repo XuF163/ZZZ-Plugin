d2b54228bcda5b7629987fd54e0a620b12644ff0
429 <15380362192@163.com>
2025-06-12 15:21:46 +0000

    Revert "去全局变量测试00可能导致miaoButton不可用"
    This reverts commit 3ce9fd0d3bf6c9e3ade1356761c4d0ddd753417d.

--- lain.support.js
+++ lain.support.js
@@ -1,10 +1,8 @@
 import settings from "./lib/settings.js"
-import { getPanelList } from "./lib/avatar.js"
-import { ZZZPlugin } from "./lib/plugin.js"
 
-export default class Button extends ZZZPlugin {
+export default class Button {
   constructor() {
-    super({
+    this.plugin = {
       name: 'zzz-plugin-Miao-support-Button',
       dsc: 'zzz-plugin button support (考古版本)',
       priority: -429,
@@ -12,29 +10,13 @@ export default class Button extends ZZZPlugin {
         { reg: '#绝区零更新面板|#绝区零面板更新|#绝区零刷新面板|#绝区零面板刷新|#绝区零更新展柜面板|#绝区零展柜面板更新$', fnc: 'profile1' },
         { reg:  '#绝区零(.*)(?:展柜)?面板(?:展柜)?(.*)$', fnc: 'handleRule' },
       ]
-    })
+    }
   }
 
-  async profile1(e) {
-    let roleList = [];
-    let ifNewChar = false;
-    
-    try {
-      // 设置事件对象
-      this.e = e;
-      
-      // 获取UID
-      const uid = await this.getUID();
-      if (uid) {
-        // 从数据库获取角色列表
-        const panelData = getPanelList(uid);
-        roleList = panelData.map(item => item.name_mi18n) || [];
-        ifNewChar = panelData.some(item => item.isNew) || false;
-      }
-    } catch (error) {
-      // 如果获取UID失败，使用默认值
-      logger.debug('获取UID失败，使用默认按钮:', error.message);
-    }
+  profile1(e) {
+
+    const roleList = global.zzzRoleList || [];
+    const ifNewChar = global.ifNewChar || false;
 
     const button = [];
 
@@ -53,6 +35,7 @@ export default class Button extends ZZZPlugin {
         label: role, callback: `%${role}面板`
       }));
       button.push(...Bot.Button(charButtonList, 4));
+    } else {
     }
 
     return button.length > 0 ? button : null;
@@ -60,25 +43,18 @@ export default class Button extends ZZZPlugin {
 
   handleRule(e) {
     let charName = '';
-    
-    // 从消息中解析角色名称
-    const match = e.match || e.msg.match(/^(%|＃|#)(.+?)(?:展柜)?面板(?:展柜)?$/);
-    const parsedName = match?.[2]?.trim();
-    if (parsedName && !['更新', '刷新', '列表'].includes(parsedName)) {
-      charName = parsedName;
+    if (global.zzzCurrentCharName) {
+      charName = global.zzzCurrentCharName;
+    } else {
+      const match = e.match || e.msg.match(/^(%|＃|#)(.+?)(?:展柜)?面板(?:展柜)?$/);
+      const parsedName = match?.[2]?.trim();
+      if (parsedName && !['更新', '刷新', '列表'].includes(parsedName)) {
+        charName = parsedName;
+      }
     }
 
-    // 如果没有角色名称，使用默认按钮
-    if (!charName) {
-      const buttonRows = [
-        [{ label: `更新面板`, callback: `%更新面板` }, { label: `展柜面板`, callback: `%更新展柜面板` }],
-        [{ label: `练度统计`, callback: `%练度统计` }, { label: `投喂`, link: settings.getConfig('config').donationLink || 'https://afdian.com/a/chickenmalon' }],
-        [{ label: `体力`, callback: `%电量` }, { label: `签到`, callback: `#签到` }]
-      ];
-      return Bot.Button(buttonRows);
-    }
 
-    // 有角色名称时的按钮布局
+
     const buttonRows = [
       [{ label: `更新面板`, callback: `%更新面板` }, { label: `展柜面板`, callback: `%更新展柜面板` }],
       [
@@ -86,12 +62,7 @@ export default class Button extends ZZZPlugin {
         { label: `练度统计`, callback: `%练度统计` },
         { label: `${charName}图鉴`, callback: `%${charName}图鉴` },
       ],
-      [
-        { label: `电量`, callback: `%体力` }, 
-        { label: `投喂`, link: settings.getConfig('config').donationLink || 'https://afdian.com/a/chickenmalon' }, 
-        { label: `${charName}伤害`, callback: `%${charName}伤害` },
-        { label: `签到`, callback: `#签到` }
-      ]
+      [{ label: `体力`, callback: `%电量` }, { label: `投喂`, link: settings.getConfig('config').donationLink || 'https://afdian.com/a/chickenmalon' }, { label: `伤害`, callback: `%${charName}伤害` },{ label: `电量`, callback: `%体力` }]
     ];
 
     return Bot.Button(buttonRows);
